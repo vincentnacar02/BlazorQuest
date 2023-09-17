eaee99604d23eaa8300c21178089ce37e7149919
Vincent Nacar <vincent.nacar@contractor.fitlg.com>
2023-09-17 11:49:36 +0800

    initial

---
+++ BlazorQuest/Data/QuestPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorQuest.Data
+{
+    public class QuestPath
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("options")]
+        public PathOptions[] Options { get; set; }
+    }
+
+    public class PathOptions
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+        [JsonPropertyName("goto")]
+        public string Goto { get; set; }
+    }
+}
---
+++ BlazorQuest/Data/QuestService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace BlazorQuest.Data
+{
+    public class QuestService
+    {
+        private IDictionary<QuestKey, string> QuestFileMap = new Dictionary<QuestKey, string>();
+        public QuestService() {
+            QuestFileMap.Add(QuestKey.EnchantedAmuletQuest, "wwwroot/enchanted-amulet-quest.json");
+            QuestFileMap.Add(QuestKey.LegendaryCrystalHeart, "wwwroot/legendary-crystal-heart.json");
+        }
+
+        public IDictionary<string, QuestPath> LoadQuest(QuestKey quest)
+        {
+            var path = QuestFileMap[quest];
+            var json = File.ReadAllText(path);
+
+            var dictionary = JsonSerializer.Deserialize<IDictionary<string, QuestPath>>(json);
+            if (dictionary == null)
+            {
+                throw new Exception("failed to load quest.");
+            }
+            return dictionary;
+        }
+    }
+
+    public enum QuestKey
+    {
+        EnchantedAmuletQuest,
+        LegendaryCrystalHeart
+    }
+}
