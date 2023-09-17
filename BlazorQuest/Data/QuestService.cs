0c081382b180dbe25a1e4faac0ea0aafec3d971c
Vincent Nacar <vincent.nacar@contractor.fitlg.com>
2023-09-17 21:02:27 +0800

    dynamically load quest

--- BlazorQuest/Data/QuestService.cs
+++ BlazorQuest/Data/QuestService.cs
@@ -4,16 +4,25 @@ namespace BlazorQuest.Data
 {
     public class QuestService
     {
-        private IDictionary<QuestKey, string> QuestFileMap = new Dictionary<QuestKey, string>();
-        public QuestService() {
-            QuestFileMap.Add(QuestKey.EnchantedAmuletQuest, "wwwroot/enchanted-amulet-quest.json");
-            QuestFileMap.Add(QuestKey.LegendaryCrystalHeart, "wwwroot/legendary-crystal-heart.json");
-            QuestFileMap.Add(QuestKey.LegendarySwordOfLight, "wwwroot/legendary-sword-of-light.json");
+        private IDictionary<QuestKey, string> _questFileMap = new Dictionary<QuestKey, string>();
+        private IDictionary<QuestKey, string> _questTitleMap = new Dictionary<QuestKey, string>();
+
+        public IDictionary<QuestKey, string> QuestTitleMap { get { return _questTitleMap; } }
+
+        public QuestService() 
+        {
+            _questFileMap.Add(QuestKey.EnchantedAmuletQuest, "wwwroot/enchanted-amulet-quest.json");
+            _questFileMap.Add(QuestKey.LegendaryCrystalHeart, "wwwroot/legendary-crystal-heart.json");
+            _questFileMap.Add(QuestKey.LegendarySwordOfLight, "wwwroot/legendary-sword-of-light.json");
+
+            _questTitleMap.Add(QuestKey.EnchantedAmuletQuest, "Finding Enchanted Amulet");
+            _questTitleMap.Add(QuestKey.LegendaryCrystalHeart, "Search for Legendary Crystal Heart");
+            _questTitleMap.Add(QuestKey.LegendarySwordOfLight, "The Legendary Sword of Light");
         }
 
         public IDictionary<string, QuestPath> LoadQuest(QuestKey quest)
         {
-            var path = QuestFileMap[quest];
+            var path = _questFileMap[quest];
             var json = File.ReadAllText(path);
 
             var dictionary = JsonSerializer.Deserialize<IDictionary<string, QuestPath>>(json);
